12ab12e8ba30130d60a4c5ee6ff15264254462fb
Harry.kouevi <harry.kouevi@gmaol.com>
2026-05-11 14:13:57 +0000

    add watchdog

--- watchdog.sh
+++ watchdog.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 
-PROJECT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT="/var/www/monprojet"
 PHP="/usr/local/php8.2/bin/php"
 LOGFILE="$PROJECT/storage/logs/queue.log"
 
-while true
-do
-    if ! pgrep -f "artisan queue:work database" > /dev/null
-    then
-        echo "$(date) - Worker arrêté. Redémarrage..." >> "$LOGFILE"
+echo "$(date) - Démarrage worker Laravel" >> "$LOGFILE"
 
-        $PHP $PROJECT/artisan queue:work database \
-        --queue=notifications,upload \
-        --sleep=3 \
-        --tries=3 \
-        --timeout=90 \
-        >> "$LOGFILE" 2>&1 &
-
-        echo "$(date) - Nouveau PID : $!" >> "$LOGFILE"
-    else
-        PID=$(pgrep -f "artisan queue:work database")
-        echo "$(date) - Worker déjà actif." >> "$LOGFILE"
-    fi
-
-  sleep 60
-done
+exec $PHP $PROJECT/artisan queue:work database \
+    --queue=notifications,upload \
+    --sleep=3 \
+    --tries=3 \
+    --timeout=90 \
+    >> "$LOGFILE" 2>&1
