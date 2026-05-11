370bff7894a5bb773d176352da21480cd87ff50b
Harry.kouevi <harry.kouevi@gmaol.com>
2026-05-11 17:01:32 +0000

    resolve cors

--- config/cors.php
+++ config/cors.php
@@ -22,7 +22,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        'https://cvmatchai.us/'
+        'https://cvmatchai.us'
     ],
 
     'allowed_origins_patterns' => [],
