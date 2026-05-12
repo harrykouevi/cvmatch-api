a3ee6c61ad45235652c7a1f5c1eb2c8859e6750b
Harry.kouevi <harry.kouevi@gmaol.com>
2026-05-12 13:10:33 +0000

    sending token

--- config/cors.php
+++ config/cors.php
@@ -23,7 +23,7 @@ return [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'https://cvmatchai.us',
-        'https://www.cvmatchai.us/'
+        'https://www.cvmatchai.us'
     ],
 
     'allowed_origins_patterns' => [],
