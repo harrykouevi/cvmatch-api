a2b86605c6fc785a5fb9ebe5079e2e823f052070
Harry.kouevi <harry.kouevi@gmaol.com>
2026-05-21 20:17:28 +0000

    changer worker

--- config/database.php
+++ config/database.php
@@ -154,9 +154,8 @@ return [
     ],
 
     'default' => [
+        'scheme' => 'tcp',
         'host' => env('REDIS_HOST', '127.0.0.1'),
-        'username' => env('REDIS_USERNAME'),
-        'password' => env('REDIS_PASSWORD'),
         'port' => env('REDIS_PORT', 6379),
         'database' => env('REDIS_DB', 0),
     ],
